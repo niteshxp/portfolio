c5757e36b9f4833de5fbc8eac7ce7a34d4eb82ea
niteshxp <niteshpatel8852@gmail.com>
2024-01-14 13:29:42 +0530

    made mobile responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,19 +2,22 @@ import React from 'react'
 import menuIcon from '../assets/hamburger.png'
 import netflix from '../assets/netflix-ui.jpg'
 import zwiggy from '../assets/zwiggyapp.jpg'
+import emailIcon from '../assets/email.png'
+import linkedinIcon from '../assets/linkedin.png'
+import githubIcon from '../assets/github.png'
 
 const Home = () => {
     return (
         <div className=''>
 
             {/* Header */}
-            <div className='w-full py-2 bg-black px-4 flex items-center justify-between'>
-                <h1 className='text-xl font-bold text-[#2967d3]'>NiteshXP</h1>
+            <div className='w-full h-20 py-2 bg-black px-4 flex items-center justify-between'>
+                <h1 className='text-4xl font-bold text-[#2967d3]'>NiteshXP</h1>
                 <img className='h-5 mx-2' src={menuIcon} alt='menu' />
             </div>
 
             {/* Intro */}
-            <div className='mt-20 h-48 flex justify-center items-center text-center'>
+            <div className='mt-36 h-48 flex justify-center items-center text-center'>
                 <div>
                     <h1 className='text-5xl font-bold py-2'>Nitesh Patel</h1>
                     <p className='font-semibold text-xl'>Full Stack Developer <br />having interest in AI</p>
@@ -22,13 +25,13 @@ const Home = () => {
             </div>
 
             {/* buttons */}
-            <div className='flex flex-col justify-center items-center'>
-                <span className='w-32 my-2 p-2 text-xl font-semibold text-center bg-pink-500 rounded-full'>Contact me</span>
+            <div className='my-4 flex flex-col justify-center items-center'>
+                <span className='w-32 my-2 p-2 text-xl font-semibold text-center bg-pink-500 rounded-full'><a href=''>Contact me</a></span>
                 <span className='w-32 my-2 p-2 text-xl font-semibold text-center border border-black rounded-full'>Resume</span>
             </div>
 
             {/* Projects */}
-            <div className='my-10'>
+            <div className='my-24'>
 
                 <div className='text-center'>
                     <h1 className='text-2xl font-semibold'>My Work</h1>
@@ -103,19 +106,27 @@ const Home = () => {
             </div>
 
             {/* Call to action */}
-            <div className='mx-5 font-semibold text-xl'>
-                <h1 className='my-2'>Lets works Together!</h1>
-                <h1 className='my-2'>And make something beautiful and great product.</h1>
+            <div id='#contact' className='mx-5 my-16 font-semibold text-xl'>
+                <h1 className='my-4'>Lets works Together!</h1>
+                <h1 className='my-4'>And make something beautiful and great product.
+                </h1>
+                <h1 className='my-2'>You can contact me at : </h1>
                 <ul className='my-5 flex justify-evenly items-center'>
-                    <li>Email</li>
-                    <li>Github</li>
-                    <li>LinkedIn</li>
+                    <li>
+                        <a href='mailto:niteshpatel8852@gmail.com'><img className='w-10' src={emailIcon} alt='email-icon' /></a>
+                    </li>
+                    <li>
+                        <a href='https://linkedin.com/in/niteshxp'><img className='w-10' src={linkedinIcon} alt='linkedin-icon' /></a>
+                    </li>
+                    <li>
+                        <a href='https://github.com/niteshxp'><img className='w-10' src={githubIcon} alt='github-icon' /></a>
+                    </li>
                 </ul>
             </div>
 
             {/* Footer */}
             <div className='mt-10 bg-pink-500 text-white text-center'>
-                <h1 className='py-4 text-2xl font-bold'>Created with ❤️ by Nitesh </h1>
+                <h1 className='py-4 text-xl font-bold'>Created with ❤️ by Nitesh </h1>
             </div>
 
 
