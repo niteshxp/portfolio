12765c21c3fe37672db85116155fbabd203260d4
niteshxp <niteshpatel8852@gmail.com>
2024-01-16 18:41:05 +0530

    Refactor Home component UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import menuIcon from '../assets/hamburger.png'
+import React, { useState } from 'react'
 import netflix from '../assets/netflix-ui.jpg'
 import zwiggy from '../assets/zwiggyapp.jpg'
 import emailIcon from '../assets/email.png'
@@ -8,34 +7,35 @@ import githubIcon from '../assets/github.png'
 import bg2 from '../assets/bg2.mp4';
 
 const Home = () => {
+
     return (
-        <div className='lg:text-white'>
+        <div className='text-white'>
 
             <div>
                 {/* Background Video */}
-                <video className='fixed top-0 left-0 w-full h-full object-cover -z-10' autoPlay loop muted>
+                <video className='fixed top-0 left-0 w-full h-full object-cover -z-20' autoPlay loop muted>
                     <source src={bg2} type='video/mp4' />
                 </video>
             </div>
 
             {/* Header */}
             <div className='w-full h-20 py-2 px-4 flex items-center justify-between'>
-                <h1 className='text-4xl font-bold text-[#2967d3]'>NiteshXP</h1>
-                <img className='h-5 mx-2' src={menuIcon} alt='menu' />
+                <h1 className='text-4xl font-bold text-gray-300'>NiteshXP</h1>
+
             </div>
 
             {/* Intro */}
             <div className='mt-36 h-48 flex justify-center items-center text-center'>
                 <div>
-                    <h1 className='text-5xl font-bold py-2'>Nitesh Patel</h1>
-                    <p className='font-semibold text-xl'>Full Stack Developer <br />having interest in AI</p>
+                    <h1 className='text-5xl py-2 px-2 rounded-lg font-bold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500'>Nitesh Patel</h1>
+                    <p className='font-semibold text-xl text-gray-200'>Full Stack Developer <br />having interest in AI</p>
                 </div>
             </div>
 
             {/* buttons */}
             <div className='my-4 flex flex-col justify-center items-center'>
-                <span className='w-32 my-2 p-2 text-xl font-semibold text-center bg-pink-500 rounded-full'><a href=''>Contact me</a></span>
-                <span className='w-32 my-2 p-2 text-xl font-semibold text-center border border-black rounded-full'>Resume</span>
+                <span className='w-32 my-2 p-2 text-xl font-semibold text-center bg-gradient-to-r from-pink-700 to-purple-700 rounded-full'><a href='https://github.com/niteshxp' target='_blank' >Contact me</a></span>
+                <span className='w-32 my-2 p-2 text-xl font-semibold text-center border-2 border-gray-400 rounded-full'>Resume</span>
             </div>
 
             {/* Projects */}
@@ -96,8 +96,8 @@ const Home = () => {
                 </h4>
 
                 <div className='my-8 flex justify-center items-center'>
-                    <span className='w-32 my-2 p-2 text-xl font-semibold text-center bg-pink-500 rounded-full'>Contact me</span>
-                    <span className='mx-5 w-32 my-2 p-2 text-xl font-semibold text-center border border-black rounded-full'>Resume</span>
+                    <span className='w-32 my-2 p-2 text-xl font-semibold text-center bg-gradient-to-r from-pink-700 to-purple-700 rounded-full'><a href='https://github.com/niteshxp' target='_blank' >Contact me</a></span>
+                    <span className='mx-5 w-32 my-2 p-2 text-xl font-semibold text-center border-2 border-gray-400 rounded-full'>Resume</span>
                 </div>
             </div>
 
